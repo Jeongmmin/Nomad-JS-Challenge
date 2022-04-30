80af94643ba55c7536405403d326e1123299e063
Jeongmmin <seo88kr@hanmail.net>
2022-04-30 23:30:07 +0900

    ✍🏻 Study Random

--- Day10/js/deletingToDos.js
+++ Day10/js/deletingToDos.js
@@ -2,22 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function deleteToDo(event) {
-    const li = event.target.parentElement;
+function deleteToDo(event) {                        // todo 지우는 함수
+    const li = event.target.parentElement;          // 어떤 것을 지워야 하는 지 알아야 한다. 우리는 click이벤트에 대한 정보가 있다. console.로 보면 path에서 버튼이 li에 포함되어 있는 것을 알 수 있음 (부모요소)
     li.remove();
+    // li.edit();
+}
+
+function editTodo(event) {
+    const li = event.target;
+    li.edit();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    // li.appendChild(span);
     span.innerText = newTodo;
-    const button = document.createElement("button");
-    button.innerText = "❌";
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
+    const button = document.createElement("button");    //button 만들기
+    button.innerText = "❌";                            // button 안에 x 이모티콘 넣기
+    button.addEventListener("click", deleteToDo);       // 버튼 이벤트 수신
+    li.appendChild(span);                               // <li><span></span></li>
+    li.appendChild(button);                             //  <li><span></span><button></button></li>              
+    toDoList.appendChild(li);                           // <ul><li><span></span><button></button></li></ul>
 }
 
 
@@ -29,3 +34,11 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+/**
+ * 삭제 버튼 만들기
+ * 이 버튼은 event를 수신해야 한다.
+ * 1. 버튼 만들기 (click event를 수신할 버튼)
+ * append는 마지막에 넣어주어야 한다.
+ * 뭔가 클릭되었다는 것을 알고 싶으면? -> evnetListener
+ */
--- Day10/js/todo.js
+++ Day10/js/todo.js
@@ -2,19 +2,33 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function paintToDo(newTodo) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    li.appendChild(span);
-    span.innerText = newTodo;
-    toDoList.appendChild(li);
+function paintToDo(newTodo) {       // todo list 만드는 함수, 인자로 newTodo를 준다.
+    const li = document.createElement("li");        // <li></li>를 변수 li로 지정
+    const span = document.createElement("span");    // <span></span>를 변수 span으로 지정
+    li.appendChild(span);                       // <li><span></span></li>, 나중에 button도 추가하기 위해서 span으로 만든다
+    span.innerText = newTodo;                   // // <li><span>newTodo</span></li>
+    toDoList.appendChild(li);                  // <ul><li><span>newTodo</span></li></ul>
 }
 
 function handleToDoSubmit(event) {
     event.preventDefault();
-    const newTodo = toDoInput.value;    // input의 현재 value를 새로운 변수에 복사하고 있음
-    toDoInput.value = "";
-    paintToDo(newTodo);
+    const newTodo = toDoInput.value;    
+    // input의 현재 value를 새로운 변수에 복사하고 있음, 복사 한 것이니까 다음에 input에    뭘해도 아무 지장이 없음
+    toDoInput.value = "";   // todo Input을 빈 상태로 만들어 준다.
+    paintToDo(newTodo);     // paintToDo에 newTodo를 주면서 호출
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+/** 
+ * 필요한 것 : form이 필요함 -> 사용자가 todo를 입력해야하고, list도 필요 사용자가 todo를 나열해야 해서
+ * 
+ * todo form, input 만들기 (required로 만든다.)
+ * 
+ * form이나까 제출될 때 새로고침 -> preventDefault() 필요함
+ * html 요소들을 js로 가져온다.
+ * fucntion 만들고 submit의 기본동작을 막을 것이다.
+ * 
+ * 해결해야 할 것
+ * 새로 고침 시 todo가 사라지지 않도록, todo 지우는 버튼 만들기
+ * */ 
